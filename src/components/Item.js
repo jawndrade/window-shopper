c750595c639c045fccda8c7ac16770ee80a05c08
Liza McLain <lizamclain@gmail.com>
2023-01-22 15:23:58 -0600

    pulled items

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,17 +1,38 @@
 import React, { useState } from 'react';
 
-export default function Item() {
+export default function Item({item}) {
 
+  const {id, name, price, image, category, description, color} = item;
     //----------->   TODO: Handle Toggle Side for Description || Dynamically Update Price Img, etc.   <---------------
 
-    const [side, setSide] = useState("front");
+    const [isBack, setIsBack] = useState(false);
 
-  return (
-    <div>
-        <img src="https://via.placeholder.com/150"/>
-        <h4>Price</h4>
+    const handleFlip = () => {
+      setIsBack(prev => !prev)
+    }
+    const front = 
+      <div>
+          <h3>{name}</h3>
+          <button>Add to Cart</button>
+          <img src={image} onClick={handleFlip}/>
+          <h4>Price: ${price}</h4>
+          <button>Like</button>
+          <button>Dislike</button>
+      </div>
+
+  const back = 
+    <div onClick={handleFlip}>
+        <h3>{name}</h3>
+        <button>Add to Cart</button>
+        <h4>{category}</h4>
+        <h4>{description}</h4>
+        <h4>{color}</h4>
+        <h4>Price: ${price}</h4>
         <button>Like</button>
         <button>Dislike</button>
     </div>
+
+  return (
+    isBack ? back : front
   )
 }
--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Item from './Item';
 
-export default function ItemList() {
+export default function ItemList({itemList}) {
+
+  const itemCards = itemList.map(item => 
+    <Item 
+      key={item.id}
+      item={item}
+    />
+  )
 
     //TODO: Render All Items in STORE.JSON
   return (
     <div>
         <h1>Item List</h1>
-        <Item/>
+        {itemCards}
     </div>
   )
 }
