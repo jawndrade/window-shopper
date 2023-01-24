ce1c3cafdef816ebe8243dfe087d7289a725931f
DylanRhinehart <dylanrhinehart@gmail.com>
2023-01-24 10:39:12 -0800

    WindowShop Item Persistence

--- src/components/WindowShop.js
+++ src/components/WindowShop.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 import Search from './Search';
 import Spotify from "react-spotify-embed";
 
-export default function WindowShop({isWindowShop, setIsWindowShop, itemList, setItemList, cartItems, setCartItems,likedItems,setLikedItems,dislikedItems,setDislikedItems}) {
+export default function WindowShop({isWindowShop, setIsWindowShop, itemList, setItemList, cartItems, setCartItems,likedItems,setLikedItems,dislikedItems,setDislikedItems, index, setIndex}) {
 
     //------------->TODO: Render Single Item || STYLE for WindowShop Mode<------------------
     const [musicShowing, setMusicShowing] = useState(true)
@@ -73,6 +73,8 @@ export default function WindowShop({isWindowShop, setIsWindowShop, itemList, set
               setLikedItems={setLikedItems}
               dislikedItems={dislikedItems}
               setDislikedItems={setDislikedItems}
+              index={index}
+              setIndex={setIndex}
             />
         </>
   )
