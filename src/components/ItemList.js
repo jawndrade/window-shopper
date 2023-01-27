335fa615a3cba541ff8d101bb94d18597b5092db
Jessica <andradejsscl@gmail.com>
2023-01-27 07:09:04 -0500

    fin

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -23,15 +23,9 @@ export default function ItemList({isWindowShop, setIsWindowShop, itemList, cartI
   )
   //Display Items -> If In windowShop mode Display by Index, If in Shop display All
   return (
-    // <br/>
     <div class="row row-cols-3 g-3" id="item-list-div">
-        {/* <h1>Item List</h1> */}
         <br/>
-      {/* <Row class="gx-5"> */}
-          {/* <Col> */}
           {isWindowShop? itemCards[index] : itemCards }
-          {/* </Col> */}
-      {/* </Row> */}
     </div>
   )
 }
